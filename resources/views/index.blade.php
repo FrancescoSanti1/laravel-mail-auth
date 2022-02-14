ee9b759d4b8eb657bc44a2ea48bb8d985d950f5c
francesco <francescosanti91@gmail.com>
2022-02-14 16:36:38 +0100

    add auth (register, login, logout)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Home videogames</title>
+</head>
+<body>
+
+    <div class="container text-center">
+        @auth
+            <h2>Ciao {{Auth::user()->name}}</h2>
+            <a class="btn btn-secondary" href="{{route('logout')}}">Logout</a>
+        @else
+            <h3>Accedi</h3>
+            <form action="{{asset('login')}}" method="post">
+                @method('post')
+                @csrf
+
+                <input class="my-2" type="email" name="email" placeholder="Inserisci la tua email"><br>
+                <input class="my-2" type="password" name="password" placeholder="Inserisci la tua password"><br>
+                <input class="my-2 btn btn-primary" type="submit" value="Accedi"><br>
+            </form>
+
+            <h3>Registrati</h3>
+            <form action="{{asset('register')}}" method="post">
+                @method('post')
+                @csrf
+
+                <input class="my-2" type="text" name="name" placeholder="Inserisci il tuo nome utente"><br>
+                <input class="my-2" type="email" name="email" placeholder="Inserisci la tua email"><br>
+                <input class="my-2" type="password" name="password" placeholder="Inserisci la tua password"><br>
+                <input class="my-2" type="password" name="password_confirmation" placeholder="Inserisci di nuovo la tua password"><br>
+                <input class="my-2 btn btn-primary" type="submit" value="Registrati"><br>
+            </form>
+        @endauth
+    </div>
+    
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
